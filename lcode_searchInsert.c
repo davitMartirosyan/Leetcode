f6bd778c75a97c882c4d1f1e13d3d50473c87b43
davitmartirosyan <phpencoding@gmail.com>
2024-03-20 00:03:29 +0400

    Search Insertion

--- lcode_searchInsert.c
+++ lcode_searchInsert.c
@@ -11,25 +11,18 @@ int searchInsert(int arr[], int size, int target)
     {
         mid = (start + end) / 2;
         if (target > arr[mid])
-        {
             start = mid + 1;
-            context = 1;
-        }
         else if (target < arr[mid])
-        {
             end = mid - 1;
-            context = -1;
-        }
         else
             return (mid);
     }
-    printf("idx: %d\n", mid);
-    return (mid);
+    return (start);
 }
 
 int main( void )
 {
-    int arr[] = {1,3,5,6};
+    int arr[] = {1, 3, 5, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
     printf("Idx: %d\n", searchInsert(arr, size, 2));
 }
