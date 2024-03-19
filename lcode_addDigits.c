51565ffdd274374529bbf358e6141c0afa368bbe
davitmartirosyan <phpencoding@gmail.com>
2024-03-19 15:48:25 +0400

    Add Digits

---
+++ lcode_addDigits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addDigits(int n)
+{
+    int s = 0;
+    while (n)
+    {
+        s += n %10;
+        n /= 10;
+    }
+    return (s);
+}
+
+int main( void )
+{
+    int num = 2563;
+    int tmp = num;
+    while (num > 9)
+        num = addDigits(num);
+    printf("Num: %d\n", num);
+}
