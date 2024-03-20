11341f93967cba1b5fab14bbc5af1967d92219df
davitmartirosyan <phpencoding@gmail.com>
2024-03-21 02:17:41 +0400

    Roman to Int

---
+++ lcode_romanToInt.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void match(char c, int *n, char roman[], int numerals[])
+{
+    int i = 0;
+
+    while (i < 7)
+    {
+        if (c == roman[i])
+            break;
+        i++;
+    }
+    *n += numerals[i];
+}
+
+int romanToInt(char *r)
+{
+    if (!r)
+        return (0);
+    int i = 0;
+    int n = 0;
+    char roman[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    int numerals[7] = {1, 5, 10, 50, 100, 500, 1000};
+    while (i < strlen(r))
+    {
+        if (r[i] == 'I' && (r[i+1] == 'V' || r[i+1] == 'X'))
+            n -= 2;
+        else if (r[i] == 'X' && (r[i+1] == 'L' || r[i+1] == 'C'))
+            n -= 20;
+        else if (r[i] == 'C' && (r[i+1] == 'D' || r[i+1] == 'M'))
+            n -= 200;
+        match(r[i], &n, roman, numerals);
+        i++;
+    }
+    return (n);
+}
+
+int main( void )
+{
+    char* r = "XXI";
+    printf("%d\n", romanToInt(r));
+}
